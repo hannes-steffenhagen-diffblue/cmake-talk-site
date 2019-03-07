c004cde8b243310fbebff365e2244bb023bae47e
Hannes Steffenhagen <hannes.steffenhagen@diffblue.com>
2019-03-07 16:19:59 +0000

    Copy reveal.js resources to site

--- site/Main.hs
+++ site/Main.hs
@@ -1,6 +1,24 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import System.FilePath.Posix(splitPath, joinPath)
 import Hakyll
+import Data.String(fromString)
+
+copyRevealResources :: Rules ()
+copyRevealResources = do
+  revealResource "css"
+  revealResource "js"
+  revealResource "lib"
+  revealResource "plugin"
+  where
+    revealResource x = match (revealPath x) $ do
+      route . customRoute $ joinPath . tail . splitPath . toFilePath
+      compile copyFileCompiler
+    revealPath = fromString . (++"/**/*") . ("reveal.js-3.7.0/"++)
 
 main :: IO ()
-main = hakyll $ return ()
+main = hakyll $ do
+  copyRevealResources
+  create ["index.html"] $ do
+    route idRoute
